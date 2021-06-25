46ac7514195aa794d72697b849e34d2e030518ff
heetbeet <example@example.com>
2021-06-25 21:47:45 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zebra-vba-packager",
+    author="Rudolf Byker, Simon Streicher",
+    author_email="rudolfbyker@gmail.com, sfstreicher@gmail.com",
+    description="A system for aggregating and mapping VBA projects from different sources into a single library ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AutoActuary/zebra-vba-packager",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+    use_scm_version={
+        'write_to': 'aa_py_xl/version.py',
+    },
+    setup_requires=[
+        'setuptools_scm',
+    ],
+    install_requires=[
+        'locate',
+    ]
+)
