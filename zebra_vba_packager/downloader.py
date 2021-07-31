25472e148c1d655c0a30412d92db2293af9cc6b2
heetbeet <sfstreicher@gmail.com>
2021-07-31 21:53:59 +0200

    Update downloader.py

--- zebra_vba_packager/downloader.py
+++ zebra_vba_packager/downloader.py
@@ -104,11 +104,8 @@ def git_download(git_source, dest, revision=None):
 
         for get_all_upstream in [False, True]:
             if get_all_upstream:
-                current_branch = sh_lines([git, "branch", "--show-current"], stderr=subprocess.DEVNULL)[0]
-                subprocess.call([sh, "-c", f"git branch -a | grep remote | grep -v HEAD | grep -v {current_branch};"])
-                subprocess.call([sh, "-c", f"for i in `git branch -a | grep remote | grep -v HEAD | grep -v {current_branch}`; "
-                                            "do git branch --track ${i#remotes/origin/} $i;"
-                                            "done"])
+                for branch in sh_lines([sh, "-c", f"git branch -a | grep remote | grep -v HEAD;"]):
+                    sh_quiet([git, "branch", "--track", branch.split("/")[-1], branch])
 
             sh_quiet([git, "fetch",  "--all"])
             sh_quiet([git, "fetch", "--tags", "--force"])
