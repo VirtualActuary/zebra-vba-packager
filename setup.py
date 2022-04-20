f431d5522b6f8774e2699ed74934945bb185f46f
heetbeet <sfstreicher@gmail.com>
2022-04-20 13:07:39 +0200

    Add requirements.txt into setup.py

--- setup.py
+++ setup.py
@@ -32,5 +32,5 @@ setuptools.setup(
     setup_requires=[
         "setuptools_scm",
     ],
-    install_requires=["locate", "download", "pathvalidate"],
+    install_requires=["locate", "download", "pathvalidate", "py7zr", "deprecation"],
 )
